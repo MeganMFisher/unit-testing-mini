aef999d2de4817fc78715ce2ff23232aada3f7a6
meganmfisher <meganmichellefisher@gmail.com>
2017-10-05 11:50:27 -0600

    completed

--- functions.test.js
+++ functions.test.js
@@ -11,7 +11,26 @@ test("greeting() should return a dynamic greeting based on name.", () => {
 })
 
 
+describe("Math functions:", () => {
+
 test("add() should return a dynamic sum based on two number parameters", ()=> {
     expect(functions.add(1,2)).toEqual(3)
     expect(functions.add(5,9)).toEqual(14)
+})
+
+test("multiply() should return a dynamic sum based on two number parameters", () => {
+    expect(functions.multiply(1,2)).toEqual(2)
+    expect(functions.multiply(2,4)).toEqual(8)
+})
+
+test("divide() should return a dynamic sum based on two number parameters", () => {
+    expect(functions.divide(8,2)).toEqual(4)
+    expect(functions.divide(10,5)).toEqual(2)
+})
+
+test("subtract() should return a dynamic sum based on two number parameters", () => {
+    expect(functions.subtract(2,1)).toEqual(1)
+    expect(functions.subtract(4,2)).toEqual(2)
+})
+
 })
