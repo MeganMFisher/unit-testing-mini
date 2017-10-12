5268fb678bf63fa5fb3d67e79a89bc00a4bb5ecb
meganmfisher <meganmichellefisher@gmail.com>
2017-10-12 13:53:54 -0600

    stocks

---
+++ stocks.test.js
@@ -0,0 +1,6 @@
+const stockData = './mockData/stockData';
+const fns = './stocks';
+
+test('reduceStocks()', function() {
+    //client wants to know exactly how much they will make at any given time.
+})
